9123958415629a4f4c4d263d7c4ed4fcc7b96ab4
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2024-04-12 09:37:23 -0400

    Ajout page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Accueil from './pages/Accueil/'
+import About from './pages/About/'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
+
+
+
+
