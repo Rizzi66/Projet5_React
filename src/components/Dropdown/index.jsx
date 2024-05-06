840d5925044d0c80eba4e493d30da934ba7aad44
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2024-05-06 16:52:58 +0200

    Finalisation dropdown transition + responsive

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,31 +1,40 @@
 import './index.scss'
+import { useState } from 'react'
 import Arrow from '../../assets/arrow_dropdown.svg'
 
+
 function Dropdown({ title, description }) {
+  const [open, setOpen] = useState(false)
 
+  function openContent() {
+    open ? setOpen(false) : setOpen(true)
+  } 
 
-    return (
-      <div className='dropdown'>
-        <div className='dropdown__menu'>
-            <h2 className='dropdown__title'>{title}</h2>
-            <button className='dropdown__button' /*onClick={handleClick}*/ >
-              <img src={Arrow} />
-            </button>
-        </div>
-        {Array.isArray(description) ? (
-          <div className="dropdown__content">
-            {description.map((element, index) => {
-              return (
-                <p key={`${element}-${index}`}>{element}</p>
-              )
-            })}
-          </div>
-        ) : (
-          <p className="dropdown__content">{description}</p>
-        )}
+  const classContent = `${open ? 'dropdown__content dropdown__content--open' : 'dropdown__content'}`
+  const classButton = `${open ? 'dropdown__button dropdown__button--open' : 'dropdown__button'}`
+
+  return (
+    <div className='dropdown'>
+      <div className='dropdown__menu'>
+          <h2 className='dropdown__title'>{title}</h2>
+          <button className={classButton} onClick={openContent} >
+            <img src={Arrow} />
+          </button>
       </div>
-    )
-  }
+      {Array.isArray(description) ? (
+        <div className={classContent}>
+          {description.map((element, index) => {
+            return (
+              <p key={`${element}-${index}`}>{element}</p>
+            )
+          })}
+        </div>
+      ) : (
+        <p className={classContent}>{description}</p>
+      )}
+    </div>
+  )
+}
   
   export default Dropdown
   
